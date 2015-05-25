11f13e19fc49e40c513f63d5db2a9bc71653dd56
diamant1991 <diamant1988@mail.ru>
2015-05-25 16:58:15 +0300

    other files

--- js/script.js
+++ js/script.js
@@ -5,6 +5,18 @@
     });
     wnd_resize()
 
+    $('.slider-tovar-show').click(function(){
+        $('#text-slider .carousel-inner > .visible-item').slideToggle(400);
+         $(this).text( $(this).text() == 'Свернуть все товары' ? "Показать все товары" : "Свернуть все товары");
+    })
+
+    $('.gallery-show').click(function(){
+        $('.gallery').find('.hidden-xs').slideToggle(400);
+        $(this).text( $(this).text() == 'Свернуть все' ? "Показать все" : "Свернуть все");
+    })
+
+
+
 });
 function wnd_resize(){
         if($(window).width()<976){
